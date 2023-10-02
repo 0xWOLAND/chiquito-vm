c15df556cc2e96344e6ff1fb1b6b46cf443263b2
bhargav <bhargav.annem@gmail.com>
2023-10-02 14:27:03 -0700

    feat: calculate register count

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
-use std::env;
-use std::fs;
+use std::{cmp, fs};
+
+use std::hash::Hash;
+
+use chiquito::frontend::dsl::circuit;
+use chiquito::plonkish::ir::assignments::AssignmentGenerator;
+use halo2curves::ff::Field;
+
+// fn vm_circuit<F: Field + From<u64> + Hash>() -> (Circuit<F>, Option<AssignmentGenerator<F, ()>>) {
+//     use chiquito::frontend::dsl::cb::*;
+
+//     let vm = circuit::<F, (), _>("vm", |ctx| {});
+// }
 
 #[derive(Debug, Clone, Copy)]
 struct Operation {
@@ -7,7 +18,13 @@ struct Operation {
     opcode: i32,
 }
 
+fn parse_hex(s: &str) -> usize {
+    usize::from_str_radix(s.strip_prefix("0x").unwrap(), 16).unwrap()
+}
+
 pub fn main() {
+    let mut memory_register_count: usize = 0;
+
     let contents = fs::read_to_string("asm/test.asm").unwrap();
     let contents = contents
         .lines()
@@ -22,6 +39,7 @@ pub fn main() {
                 .map(|s| s.to_string())
                 .collect::<Vec<String>>();
             let argument_count = _op.len() - 1;
+            let args: Vec<String> = _op[1..].to_vec();
             let _op = _op[0].to_owned();
             let op = match _op.as_ref() {
                 "set" => Operation {
@@ -55,8 +73,32 @@ pub fn main() {
                 "Invalid number of arguments. Expected {} received {} for {}",
                 op.argument_count, argument_count, _op
             );
+
+            memory_register_count = match _op.as_ref() {
+                "set" | "out" => cmp::max(memory_register_count, parse_hex(&args[0])),
+                _ => cmp::max(
+                    memory_register_count,
+                    args.iter()
+                        .map(|x| parse_hex(&x))
+                        .fold(0, |high, x| cmp::max(high, x)),
+                ),
+            };
             op
         })
         .collect::<Vec<Operation>>();
+    println!("memory register count: {}", memory_register_count);
     println!("{:?}", contents);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parse_hex;
+
+    #[test]
+    fn check_hex_parsing() {
+        (0..10).into_iter().for_each(|x| {
+            let hex = format!("0x{}", x);
+            assert_eq!(parse_hex(&hex), x);
+        })
+    }
 }
